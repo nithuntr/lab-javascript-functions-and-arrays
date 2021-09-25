b9f4133dc7cfce69ea97c09488a0a940da31ba78
nithuntr <nithuntr19@gmail.com>
2021-09-25 15:22:04 +0530

    functions

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,90 @@
 // Progression #1: Greatest of the two numbers
-
+function greatestOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
 // Progression #2: The lengthy word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+function findScaryWord(words) {
+  if(words.length == 0)
+  {
+    return null
+  }
+  else
+  {
+    let longW = "";
+    let maxid = 0;
+    for(let i=0; i<words.length; i++)
+    {
+      if (words[i].length>maxid)
+      {
+        maxid = words[i].length
+        longW = words[i]
+      }
+  }
+  return longW
+}
+}
+
 
 // Progression #3: Net Price
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function netPrice(numbers) {
+  let sum = 0
+  for(let i = 0 ; i < numbers.length ; i++)
+  {
+    sum += numbers[i] 
+  }
+  return sum
+}
+
+function add(numbersAvg) {
+  if(!numbersAvg.length) return 0
+  let sum = 0 
+  for(let i = 0 ; i < numbersAvg.length ; i++)
+  {
+    if(typeof(numbersAvg[i]) == "string")
+      {
+        sum += numbersAvg[i].length
+      }
+      else if(typeof(numbersAvg[i]) == "number" || typeof(numbersAvg[i]) == "boolean")
+      {
+        sum += numbersAvg[i]
+      }
+      else
+      {
+        throw new Error('Unsupported data type sir or ma\'am')
+      } 
+    }
+    return sum
+  }
+
 
 // Progression #4: Calculate the average
 // Progression 4.1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
-
+function midPointOfLevels(numbersArr) {
+  if (numbersArr.length==0) return null 
+  else return add(numbersArr)/numbersArr.length 
+}
 // Progression 4.2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+function averageWordLength(wordsArr) {
+  if (wordsArr.length==0) return null 
+  else return add(wordsArr)/wordsArr.length 
+ 
+}
 
+function avg(arr) {
+  let avg = averageWordLength(arr);
+  if (avg) {
+    return parseFloat(avg.toFixed(2));
+  }
+  return null;
+}
 // Progression #5: Unique arrays
 const wordsUnique = [
   'bread',
@@ -28,10 +100,38 @@ const wordsUnique = [
   'egg',
   'flour'
 ];
+function uniqueArray(arr) {
 
+  let uniqueArr = []
+  for (let i of arr) {
+    let index = arr.indexOf(i)
+    if (uniqueArr.indexOf(index) == -1) {
+      uniqueArr.push(index)
+    }
+  }
+  if (!uniqueArr.length) {
+    return null
+  }
+  let uniqueArrList = [];
+
+  for (let i of uniqueArr) {
+    uniqueArrList.push(arr[i])
+  }
+  return uniqueArrList
+}
 // Progression #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+function searchElement(arr, word) {
+  if (!arr.length) {
+    return null
+  }
+  for (let i = 0 ; i < arr.length ; i++) {
+    if (arr[i] == word) {
+      return true
+    }
+  }
 
+}
 // Progression #7: Count repetition
 const wordsCount = [
   'machine',
@@ -46,7 +146,18 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
-
+function howManyTimesElementRepeated(arr, search) {
+  if (!arr.length) {
+    return 0
+  }
+  let count = 0
+  for (let i = 0 ; i < arr.length ; i++) {
+    if (arr[i] == search)  {
+      count += 1
+    }
+  }
+  return count
+}
 // Progression #8: Bonus
 
 const matrix = [
@@ -61,3 +172,34 @@ const matrix = [
   [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
   [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
 ];
+
+
+function maximumProduct(matrix) {
+  let result = 0;
+
+  for (let i = 0; i <= matrix.length - 4; i++) {
+    let P1 = 1;
+    let P2 = 1;
+    for (let k = 0; k < matrix[0].length; k++) {
+
+      for (let j = k; j <= k + 3; j++) {
+        if (matrix[i][j]) {
+          P1 *= matrix[i][j];
+        }
+      }
+
+
+      for (let j = 0; j <= 3; j++) {
+        P2 *= matrix[i + j][k];
+        console.log(matrix[i + j][k]);
+      }
+
+      if (P1 > P2) {
+        result = P1;
+      } else {
+        result = P2;
+      }
+    }
+  }
+  return result;
+}
